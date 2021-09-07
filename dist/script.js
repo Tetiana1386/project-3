ce00dfe8af9d1fea25385741aa46f730510c8798
Tetiana1386 <tanyashostak13@gmail.com>
2021-09-07 22:27:30 +0300

    fix slider

--- dist/script.js
+++ dist/script.js
@@ -930,12 +930,12 @@ module.exports = g;
 
 "use strict";
 __webpack_require__.r(__webpack_exports__);
-/* harmony import */ var _modules_slider__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./modules/slider */ "./src/js/modules/slider.js");
+!(function webpackMissingModule() { var e = new Error("Cannot find module './modules/slider'"); e.code = 'MODULE_NOT_FOUND'; throw e; }());
 /* harmony import */ var _modules_playVideo__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ./modules/playVideo */ "./src/js/modules/playVideo.js");
 
 
 window.addEventListener('DOMContentLoaded', function () {
-  var slider = new _modules_slider__WEBPACK_IMPORTED_MODULE_0__["default"]('.page', '.next');
+  var slider = new !(function webpackMissingModule() { var e = new Error("Cannot find module './modules/slider'"); e.code = 'MODULE_NOT_FOUND'; throw e; }())('.page', '.next');
   slider.render();
   var player = new _modules_playVideo__WEBPACK_IMPORTED_MODULE_1__["default"]('.showup .play', '.overlay');
   player.init();
@@ -1026,86 +1026,6 @@ function () {
   }]);
 
   return VideoPlayer;
-}();
-
-
-
-/***/ }),
-
-/***/ "./src/js/modules/slider.js":
-/*!**********************************!*\
-  !*** ./src/js/modules/slider.js ***!
-  \**********************************/
-/*! exports provided: default */
-/***/ (function(module, __webpack_exports__, __webpack_require__) {
-
-"use strict";
-__webpack_require__.r(__webpack_exports__);
-/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "default", function() { return Slider; });
-/* harmony import */ var core_js_modules_web_dom_collections_for_each__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! core-js/modules/web.dom-collections.for-each */ "./node_modules/core-js/modules/web.dom-collections.for-each.js");
-/* harmony import */ var core_js_modules_web_dom_collections_for_each__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(core_js_modules_web_dom_collections_for_each__WEBPACK_IMPORTED_MODULE_0__);
-
-
-function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
-
-function _defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } }
-
-function _createClass(Constructor, protoProps, staticProps) { if (protoProps) _defineProperties(Constructor.prototype, protoProps); if (staticProps) _defineProperties(Constructor, staticProps); return Constructor; }
-
-var Slider =
-/*#__PURE__*/
-function () {
-  function Slider(page, btns) {
-    _classCallCheck(this, Slider);
-
-    this.page = document.querySelector(page);
-    this.slides = this.page.children;
-    this.btns = document.querySelectorAll(btns);
-    this.slideIndex = 1;
-  }
-
-  _createClass(Slider, [{
-    key: "showSlides",
-    value: function showSlides(n) {
-      if (n > this.slides.length) {
-        this.slideIndex = 1;
-      }
-
-      if (n < 1) {
-        this.slideIndex = this.slides.length;
-      }
-
-      this.slides.forEach(function (slide) {
-        slide.style.display = 'none';
-      });
-      this.slides[this.slideIndex - 1].style.display = 'block';
-    }
-  }, {
-    key: "plusSlides",
-    value: function plusSlides(n) {
-      this.showSlides(this.slideIndex += n);
-    }
-  }, {
-    key: "render",
-    value: function render() {
-      var _this = this;
-
-      this.btns.forEach(function (item) {
-        item.addEventListener('click', function () {
-          _this.plusSlides(1);
-        });
-        item.parentNode.previousElementSibling.addEventListener('click', function (e) {
-          e.preventDefault();
-          _this.slideIndex = 1;
-
-          _this.showSlides(_this.slideIndex);
-        });
-      });
-      this.showSlides(this.slideIndex);
-    }
-  }]);
-
-  return Slider;
 }();
 
 
