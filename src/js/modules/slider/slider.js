3e1089ae7b5bf8d51992e6c1caef93ab13a39f38
Tetiana1386 <tanyashostak13@gmail.com>
2021-09-12 00:00:58 +0300

    added mini-sliders

--- src/js/modules/slider/slider.js
+++ src/js/modules/slider/slider.js
@@ -1,8 +1,14 @@
 export default class Slider {
-    constructor({ container = "", btns = "", next = "", prev = "" } = {}) {
+    constructor({ container = null, btns = null, next = null, prev = null, activeClass = '',
+        animate, autoplay } = {}) {
         this.container = document.querySelector(container);
-        this.slides = this.page.children;
+        this.slides = this.container.children;
         this.btns = document.querySelectorAll(btns);
+        this.prev = document.querySelector(prev);
+        this.next = document.querySelector(next);
+        this.activeClass = activeClass;
+        this.animate = animate;
+        this.autoplay = autoplay;
         this.slideIndex = 1;
     }
 }
